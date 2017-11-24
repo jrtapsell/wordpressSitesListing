92384ef5980f6ac01c852e987dafcd06c6f98457
James Tapsell <email@jrtapsell.co.uk>
2017-11-24 12:26:40 +0000

    Plus != Dot in PHP

--- wordpressSitesListing.php
+++ wordpressSitesListing.php
@@ -5,8 +5,8 @@ Plugin Name: Wordpress Sites Listing
 function multisites($args) {
     $data = '';
     foreach(get_sites() as $site) {
-        $data = $data + json_encode($site);
-        $data = $data + '<br>';
+        $data = $data . json_encode($site);
+        $data = $data . '<br>';
     }
     return $data;
 }
